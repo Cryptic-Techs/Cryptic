7a13ad76fa91a1a547a640365361e958a957989b
Wixonic <66529056+Wixonic@users.noreply.github.com>
2021-01-17 16:46:53 +0100

    Resolve Watchers Count Bug

--- js/index.js
+++ js/index.js
@@ -2,17 +2,21 @@ const init = () =>
 {
 	return new Promise(resolve =>
 	{
-		request("https://api.github.com/orgs/Cryptic-Techs/repos").then(repos => {
+		request("https://api.github.com/orgs/Cryptic-Techs/repos").then(repos =>
+		{
 			repos = JSON.parse(repos);
 			
 			for (let x in repos)
 			{
-				const repo = GITHUB.getRepoDatas(repos[x]);
-				
-				if (!repo.forked)
+				request(`https://api.github.com/repos/${repos[x].full_name}`).then(repo =>
 				{
-					doc.id("repos").innerHTML += `<a href="${repo.url}" target="_blank" class="repo"><h3>${repo.name}</h3><bar><div><i class="far fa-star"></i> ${repo.stars}</div><div><i class="far fa-eye"></i> ${repo.watchers}</div><div><i class="fas fa-exclamation-circle"></i> ${repo.issues}</div><div><i class="fas fa-code-branch"></i> ${repo.forks}</div><div><i class="fas fa-${repo.public ? "unlock" : "lock"}"></i></div></bar><p>${repo.description}</p></a>`;
-				}
+					repo = GITHUB.getRepoDatas(repo);
+					
+					if (!repo.forked)
+					{
+						doc.id("repos").innerHTML += `<a href="${repo.url}" target="_blank" class="repo"><h3>${repo.name}</h3><bar><div><i class="far fa-star"></i> ${repo.stars}</div><div><i class="far fa-eye"></i> ${repo.watchers}</div><div><i class="fas fa-exclamation-circle"></i> ${repo.issues}</div><div><i class="fas fa-code-branch"></i> ${repo.forks}</div><div><i class="fas fa-${repo.public ? "unlock" : "lock"}"></i></div></bar><p>${repo.description}</p></a>`;
+					}
+				});
 			}
 			
 			resolve();
